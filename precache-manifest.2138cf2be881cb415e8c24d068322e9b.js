19aab5efcc9753e46d30eb450d5d1de620713395
NNaattaa <50961364+NNaattaa@users.noreply.github.com>
2020-12-03 23:45:53 +0200

    Updates

---
+++ precache-manifest.2138cf2be881cb415e8c24d068322e9b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1604ef6ca91c3456a022706ad9fb1c33",
+    "url": "/goit-react-hw-08-phonebook_/index.html"
+  },
+  {
+    "revision": "8e627a2023b17d01ebb2",
+    "url": "/goit-react-hw-08-phonebook_/static/css/3.bec92170.chunk.css"
+  },
+  {
+    "revision": "d871b9b4cefb2a105411",
+    "url": "/goit-react-hw-08-phonebook_/static/css/4.4000481a.chunk.css"
+  },
+  {
+    "revision": "e4e7c4ca07128b8e566b",
+    "url": "/goit-react-hw-08-phonebook_/static/css/5.232161e2.chunk.css"
+  },
+  {
+    "revision": "c48f524b81fca42dfe14",
+    "url": "/goit-react-hw-08-phonebook_/static/css/7.171c5e20.chunk.css"
+  },
+  {
+    "revision": "500da71239544cb356d2",
+    "url": "/goit-react-hw-08-phonebook_/static/css/main.f4f24826.chunk.css"
+  },
+  {
+    "revision": "571047fb139b56139533",
+    "url": "/goit-react-hw-08-phonebook_/static/js/2.77d492b6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-08-phonebook_/static/js/2.77d492b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e627a2023b17d01ebb2",
+    "url": "/goit-react-hw-08-phonebook_/static/js/3.a9252a44.chunk.js"
+  },
+  {
+    "revision": "d871b9b4cefb2a105411",
+    "url": "/goit-react-hw-08-phonebook_/static/js/4.ea7c683a.chunk.js"
+  },
+  {
+    "revision": "e4e7c4ca07128b8e566b",
+    "url": "/goit-react-hw-08-phonebook_/static/js/5.2f936837.chunk.js"
+  },
+  {
+    "revision": "088a8a558c63f5203481",
+    "url": "/goit-react-hw-08-phonebook_/static/js/6.7e083f57.chunk.js"
+  },
+  {
+    "revision": "c48f524b81fca42dfe14",
+    "url": "/goit-react-hw-08-phonebook_/static/js/7.d75b174d.chunk.js"
+  },
+  {
+    "revision": "500da71239544cb356d2",
+    "url": "/goit-react-hw-08-phonebook_/static/js/main.da000daa.chunk.js"
+  },
+  {
+    "revision": "23d49801846d794eda85",
+    "url": "/goit-react-hw-08-phonebook_/static/js/runtime-main.e7d34097.js"
+  }
+]);
